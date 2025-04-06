2ced1d6218abf1ae51911e216dead97ebfee8e16
Alex-Yu21 <yualex2112@gmail.com>
2025-04-06 05:37:27 +0400

    refactor: optimize MainScreen structure and logic

--- lib/presentation/screens/main_screen.dart
+++ lib/presentation/screens/main_screen.dart
@@ -8,17 +8,21 @@ import 'package:rick_and_morty/presentation/providers/bottom_nav_provider.dart';
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
+  static final List<Widget> _screens = [
+    const CharacterListScreen(),
+    const FavoriteScreen(),
+  ];
+
+  static const List<String> _titles = ['Characters', 'Favorites'];
+
   @override
   Widget build(BuildContext context) {
-    final bottomNavProvider = context.watch<BottomNavProvider>();
-
-    final screens = [CharacterListScreen(), FavoriteScreen()];
-    final titles = ['Characters', 'Favorites'];
+    final currentIndex = context.watch<BottomNavProvider>().currentIndex;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          titles[bottomNavProvider.currentIndex],
+          _titles[currentIndex],
           style: const TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
@@ -32,16 +36,14 @@ class MainScreen extends StatelessWidget {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: bottomNavProvider.currentIndex,
-        onTap: (val) {
-          context.read<BottomNavProvider>().setIndex(val);
-        },
+        currentIndex: currentIndex,
+        onTap: context.read<BottomNavProvider>().setIndex,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
           BottomNavigationBarItem(icon: Icon(Icons.star), label: ''),
         ],
       ),
-      body: screens[bottomNavProvider.currentIndex],
+      body: _screens[currentIndex],
     );
   }
 }
